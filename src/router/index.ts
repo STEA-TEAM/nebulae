9f106601f7684f3b3d56c59e7a60c4b68f1669d4
ParticleG <particle_g@outlook.com>
2023-06-21 00:19:59 +0800

    - Fix warnings

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,9 +20,11 @@ import routes from './routes';
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
+    : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory;
 
-  const Router = createRouter({
+  return createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
 
@@ -31,6 +33,4 @@ export default route(function (/* { store, ssrContext } */) {
     // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE),
   });
-
-  return Router;
 });
